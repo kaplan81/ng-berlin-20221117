d2c3285d541b905da9b19f76c94b1c1a8c96726a
“kaplan81” <gesteira.pro@gmail.com>
2022-11-16 15:42:17 +0100

    fix error

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -1,18 +1,28 @@
+// @ts-check
+
 const {
   shareAll,
   withModuleFederationPlugin,
 } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
-  name: 'mfe1',
-  exposes: {
-    './Component': './projects/mfe1/src/app/app.component.ts',
-  },
-  shared: {
-    ...shareAll({
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: 'auto',
-    }),
-  },
-});
+const mfe1WebpackConfig = {
+  ...withModuleFederationPlugin({
+    name: 'mfe1',
+    exposes: {
+      './Component': './projects/mfe1/src/app/app.component.ts',
+    },
+    shared: {
+      ...shareAll({
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto',
+      }),
+    },
+  }),
+};
+
+// Uncomment for debugging.
+// console.log('mfe1WebpackConfig:::', mfe1WebpackConfig);
+// console.log('ModuleFederationPlugin:::', mfe1WebpackConfig.plugins[0]);
+
+module.exports = mfe1WebpackConfig;
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,17 +1,36 @@
+// @ts-check
+
 const {
   shareAll,
   withModuleFederationPlugin,
 } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
-  remotes: {
-    mfe1: 'http://localhost:3000/remoteEntry.js',
+const shellWebpackConfig = {
+  ...withModuleFederationPlugin({
+    remotes: {
+      mfe1: 'http://localhost:3000/remoteEntry.js',
+    },
+    shared: {
+      ...shareAll({
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto',
+      }),
+    },
+  }),
+  /**
+   * Fix reference:
+   * https://github.com/angular-architects/module-federation-plugin/issues/96#issuecomment-981896034
+   */
+  output: {
+    uniqueName: 'shell',
+    publicPath: 'auto',
+    scriptType: 'text/javascript',
   },
-  shared: {
-    ...shareAll({
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: 'auto',
-    }),
-  },
-});
+};
+
+// Uncomment for debugging.
+// console.log('shellWebpackConfig:::', shellWebpackConfig);
+// console.log('ModuleFederationPlugin:::', shellWebpackConfig.plugins[0]);
+
+module.exports = shellWebpackConfig;
