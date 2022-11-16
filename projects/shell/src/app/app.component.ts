712751dd7e31ee9a3aa65695956ba527edc74664
“kaplan81” <gesteira.pro@gmail.com>
2022-11-16 13:46:22 +0100

    make ng serve work

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'she-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterModule],
 })
 export class AppComponent {
   title = 'shell';
