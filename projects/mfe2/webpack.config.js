e8211daa83697dbc0ade84672391d04d5cc5b87f
kaplan81 <gesteira.pro@gmail.com>
2022-11-17 03:53:36 +0100

    refine webpack config for mfe2

--- projects/mfe2/webpack.config.js
+++ projects/mfe2/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const deps = require('./package.json').dependencies;
 
 module.exports = {
   entry: './src/index.js',
@@ -9,18 +8,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-    }),
-    new ModuleFederationPlugin({
-      name: 'mfe2',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App',
-      },
-    }),
-  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'build'),
@@ -44,4 +31,16 @@ module.exports = {
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new ModuleFederationPlugin({
+      name: 'mfe2',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',
+      },
+    }),
+  ],
 };
